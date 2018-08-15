1115afd191dc1dfbfa7dfdc89a42aa471b18e0a2
chenwei-cw <435482519@qq.com>
2018-08-15 15:19:26 +0800

    修改

--- build/webpack.pro.js
+++ build/webpack.pro.js
@@ -3,8 +3,8 @@ const common = require('./webpack.common.js');
 const webpack = require('webpack');
 
 const devConfig = {
-    mode: 'production',  //process.env.NODE_ENV 的值设为 development。启用 NamedChunksPlugin 和 NamedModulesPlugin。
-    // devtool: 'inline-source-map', //
+    mode: 'development',  //process.env.NODE_ENV 的值设为 development。启用 NamedChunksPlugin 和 NamedModulesPlugin。
+    devtool: 'inline-source-map', //
 }
 
 module.exports = merge(common,devConfig);//合并并导出dev和common文件`
